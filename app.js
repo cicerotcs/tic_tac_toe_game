dc67d2533b8f0462235feb8e4f9d801911090946
Cicero Teixeira <ccrtxr@gmail.com>
2023-02-22 15:56:19 +1030

    implemented the feature which the player can choose x or o via css and js

--- app.js
+++ app.js
@@ -1,17 +1,86 @@
 const boxes = document.querySelectorAll(".box");
 const checkbox = document.querySelector("input");
 const label = document.querySelector("label");
+const player2 = document.querySelector(".player2");
+const computer = document.querySelector(".computer");
+const ticChoice = document.querySelector(".tic-choice");
 
-function chooseTic(tic) {
-  return tic;
+const player1Tic = document.querySelector(".player1 .info-tic");
+const player2Tic = document.querySelector(".player2 .info-tic");
+const computerTic = document.querySelector(".computer .info-tic");
+
+const ticX = document.querySelector(".tic-x");
+const ticO = document.querySelector(".tic-o");
+
+let playersInfo = {
+  player1: {
+    score: 0,
+    tic: null,
+  },
+  player2: {
+    score: 0,
+    tic: null,
+  },
+  computer: {
+    score: 0,
+    tic: null,
+  },
+};
+
+function chooseTic(event) {
+  let playerTic = event.target;
+  setTimeout(() => {
+    ticChoice.style = "display: none";
+  }, 1000);
+  if (playerTic.dataset.choice === "x") {
+    computerTic.innerHTML = ticO.dataset.choice;
+  } else {
+    computerTic.innerHTML = ticX.dataset.choice;
+  }
+  player1Tic.innerHTML = playerTic.dataset.choice;
+}
+
+function generateTicForEachPlayer() {
+  if (checkbox.checked) {
+    const tic = Math.floor(Math.random() * 2);
+
+    if (tic === 0) {
+      playersInfo.player1.tic = "o";
+      playersInfo.player2.tic = "x";
+    } else {
+      playersInfo.player1.tic = "x";
+      playersInfo.player2.tic = "o";
+    }
+  } else {
+    playersInfo.player1.tic = null;
+    playersInfo.player2.tic = null;
+    //chooseTic();
+  }
+
+  player1Tic.innerHTML = playersInfo.player1.tic;
+  player2Tic.innerHTML = playersInfo.player2.tic;
 }
 
 function numOfPlayers(players) {
   return players;
 }
 
+ticX.addEventListener("click", chooseTic);
+ticO.addEventListener("click", chooseTic);
+
 checkbox.addEventListener("change", () => {
-  checkbox.checked ? (label.innerText = "2P") : (label.innerText = "1P");
+  if (checkbox.checked) {
+    label.innerText = "2P";
+    computer.style = "display:none";
+    player2.style = "display:inline";
+    ticChoice.style = "display:none";
+  } else {
+    label.innerText = "1P";
+    computer.style = "display: inline";
+    player2.style = "display:none";
+    ticChoice.style = "display:inline";
+  }
+  generateTicForEachPlayer();
 });
 
 boxes.forEach((box) => {
